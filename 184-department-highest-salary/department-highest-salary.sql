e8870197a7bd09b0370ede1af35e55327a659871
Yatharth2808 <89832590+Yatharth2808@users.noreply.github.com>
2026-02-11 22:29:29 -0700

    Time: 611 ms (73.94%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,15 +1,11 @@
-SELECT
-  d.name AS Department,
-  e.name AS Employee,
-  e.salary AS Salary
-FROM Employee e
-JOIN Department d
-  ON e.departmentId = d.id
-JOIN (
+SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+FROM (
   SELECT departmentId, MAX(salary) AS max_salary
   FROM Employee
   GROUP BY departmentId
-) mx
-  ON mx.departmentId = e.departmentId
- AND mx.max_salary = e.salary;
-
+) xd
+JOIN Employee e
+  ON e.departmentId = xd.departmentId
+ AND e.salary = xd.max_salary
+JOIN Department d
+  ON d.id = e.departmentId;
