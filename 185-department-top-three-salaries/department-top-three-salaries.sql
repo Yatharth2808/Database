ee79cc88c0d706967ca965dfc7c33654f9919874
Yatharth2808 <89832590+Yatharth2808@users.noreply.github.com>
2026-02-11 23:00:36 -0700

    Time: 886 ms (69.07%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -2,7 +2,7 @@
 Select d.name as Department, e.name as Employee, e.salary as Salary
 from
 (
-    Select *, dense_rank() over (Partition by departmentId order by salary desc) as dr
+    Select name, salary, departmentId, dense_rank() over (Partition by departmentId order by salary desc) as dr
     from employee 
 ) e
 join department d on
