60aaa968eb16fc43eb62136b9769b89a48344f9c
Yatharth2808 <89832590+Yatharth2808@users.noreply.github.com>
2026-02-11 22:49:48 -0700

    Time: 954 ms (49.08%) | Memory: 0B (100.00%) - LeetSync

---
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+Select d.name as Department, e.name as Employee, e.salary as Salary
+from
+(
+    Select *, dense_rank() over (Partition by departmentId order by salary desc) as dr
+    from employee 
+) e
+join department d on
+e.departmentId = d.id
+where dr in (1,2,3)
