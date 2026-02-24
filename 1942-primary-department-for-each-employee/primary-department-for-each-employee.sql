ed8196569d54aec3f25fe001c20fbdd712ebf824
Yatharth2808 <89832590+Yatharth2808@users.noreply.github.com>
2026-02-24 04:45:53 -0700

    Time: 510 ms (75.87%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,16 +1,4 @@
 -- Employees with explicit primary flag
-SELECT employee_id, department_id
-FROM Employee
-WHERE primary_flag = 'Y'
-
-UNION
-
--- Employees with only one department
-SELECT employee_id, department_id
-FROM Employee
-WHERE employee_id IN (
-    SELECT employee_id
-    FROM Employee
-    GROUP BY employee_id
-    HAVING COUNT(*) = 1
-);
+# Write your MySQL query statement below
+SELECT employee_id, department_id FROM Employee
+WHERE primary_flag = "Y" OR employee_id in (SELECT employee_id FROM Employee GROUP BY employee_id HAVING COUNT(department_id) = 1)
